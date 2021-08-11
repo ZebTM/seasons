004eb166ced09201146e1dc27ccc5d4245852008
Zachary Brown <zacharybrown@tinyhomeconsultingllc.com>
2021-08-11 16:40:10 -0400

    Addition of life cycle methods

--- src/index.js
+++ src/index.js
@@ -3,21 +3,29 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor(props) {
         super(props); // Have to do this everytime
-
-        this.state = { lat: null };
+        // This is the only time we do a direct assignment
+        this.state = { lat: null, errorMessage: '' };
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 // Called setState!!!! 
                 this.setState({ lat: position.coords.latitude })
             },
-            (err) =>console.log(err) 
+            err => {
+                this.setState( { errorMessage: err.message })
+            }
         );
     }
     
+    
     render() {
-        
-         
-        return <div>Latitude: {this.state.lat}</div>
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        } 
+
+        return <div>Loading!</div>;
     }
 }
 
