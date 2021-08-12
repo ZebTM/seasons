352a825fa4c79533511397669e62774a3fb60605
Zachary Brown <zacharybrown@tinyhomeconsultingllc.com>
2021-08-12 11:33:08 -0400

    Added CSS and icons

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 class App extends React.Component {
-    constructor(props) {
-        super(props); // Have to do this everytime
-        // This is the only time we do a direct assignment
-        this.state = { lat: null, errorMessage: '' };
+    state = { lat: null, errorMessage: ''};
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                // Called setState!!!! 
-                this.setState({ lat: position.coords.latitude })
-            },
-            err => {
-                this.setState( { errorMessage: err.message })
-            }
-        );
+            position => this.setState({ lat: position.coords.latitude }), err => this.setState( { errorMessage: err.message }));
     }
-    
     
     render() {
         if(this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat}/>;
         } 
 
         return <div>Loading!</div>;
